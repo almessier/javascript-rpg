0e12415509c82baa7a0c8aca6095b78d6287f44f
Alexander Messier <alexander.j.messier@gmail.com>
2021-08-27 16:26:40 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Hercules = {
+    health = 100,
+    attackPower = 25,
+    attacks = ['Swift Slash', 'Vicious Thrust', 'Hilt Bash', 'Leaping Slam']
+}
+const lernaeanHydra = {
+    health = 100,
+    attackPower = 25,
+    attacks = ['Serpentine Strike', 'Poisonous Fumes', 'Venomous Bite', 'Tail Whip']
+}
+

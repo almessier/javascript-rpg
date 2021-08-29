37acfb1977fe87bd1e9dfc9330d944c664d5f732
Alexander Messier <alexander.j.messier@gmail.com>
2021-08-29 17:07:37 -0500

    Added attack selection function

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 'use strict'
 
-const Hercules = {
-    health = 100,
-    attackPower = 25,
-    attacks = ['Swift Slash', 'Vicious Thrust', 'Hilt Bash', 'Leaping Slam']
+const hercules = {
+    health : 100,
+    attackPower : 25,
+    attacks : ['Swift Slash', 'Vicious Thrust', 'Hilt Bash', 'Leaping Slam']
 }
 const lernaeanHydra = {
-    health = 100,
-    attackPower = 25,
-    attacks = ['Serpentine Strike', 'Poisonous Fumes', 'Venomous Bite', 'Tail Whip']
+    health : 100,
+    attackPower : 25,
+    attacks : ['Serpentine Strike', 'Poisonous Fumes', 'Venomous Bite', 'Tail Whip', 'Constriction']
 }
 
+
+playGame();
+function playGame() {
+    alert('Welcome! In this game you play as Hercules on his quest to slay the Lernaean Hydra.');
+    alert('story')
+    selectAttack();
+}
+
+
+function selectAttack(array) {
+    let attack = prompt('Type 1, 2, 3, or 4 to attack: 1. Swift Slash, 2. Vicious Thrust, 3. Hilt Bash, 4. Leaping Slam');
+    for (let i = 0; i < array.length; i++) {
+        attack = array[i];
+    }
+}

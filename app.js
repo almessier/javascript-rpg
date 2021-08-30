25bd937fe37ac511a4a5d82bd6bf3496820a9625
Alexander Messier <alexander.j.messier@gmail.com>
2021-08-29 23:27:20 -0500

    Added in the story for the playGame function

--- app.js
+++ app.js
@@ -18,16 +18,24 @@ const lernaeanHydra = {
     attackPower : 30,
     attacks : ['Serpentine Strike', 'Poisonous Fumes', 'Venomous Bite', 'Tail Whip', 'Constriction']
 }
+const hades = {
+    name: 'Hades',
+    health : 90,
+    attackPower : 20,
+    attacks : ['Titanic Slam', 'Bident Thrust']
+}
 
 playGame(hercules, nemeanLion, lernaeanHydra);
 
-function playGame(user, enemy1, enemy2) {
-    alert('Welcome! In this game you play as Hercules on his quest to slay the Lernaean Hydra.');
-    alert('story');
+function playGame(user, enemy1, enemy2, enemy3) {
+    alert('Welcome! In this game you play as Hercules. You are tasked by King Eurystheus to slay the impossible nine-headed Lernaean Hydra and capture the guard dog of the underworld, Cerberus.');
+    alert('Before you go to slay the Hydra, you hear of a vicious lion in Nemea. You have stalked it, and trapped it in a cave. Now it is time to fight it!');
     attack(user, enemy1);
-    alert('story');
+    alert('With the Nemean Lion slayed, you set out to slay the Lernaean Hydra. You cover your face to protect from it\s poisonous fumes and run into battle!');
     attack(user, enemy2);
-    alert('story');
+    alert('With the Hydra defeated, you have one final task. You must capture Hades\' dog, Cerberus, from the underworld. Hades won\'t let him go without a fight though!');
+    attack(user, enemy3);
+    alert('After being defeated, Hades allows you to take Cerberus. King Eurystheus is satisfied with your labors. You have won!');
 }
 function promptAttack() {
     let attack = prompt('Type 1, 2, 3, or 4 to attack: 1. Swift Slash, 2. Vicious Thrust, 3. Hilt Bash, 4. Leaping Slam');
@@ -59,7 +67,7 @@ function attack(user, enemy) {
         console.log(`${enemy.name} has dealt ${enemyAttackPower} damage to you with ${enemyAttackName}. You now have ${health} health.`);
     }
     if (health > 0) {
-        alert('Enemy Slain!');
+        alert('Enemy Defeated!');
     } else {
         alert('You have died, try again!');
         attack(user, enemy);
